1d4272cf1ae27352a56c0ff530f0d03558be75fb
oooielp <156877391+oooielp@users.noreply.github.com>
2026-04-03 21:08:40 -0400

    fixes cvar not registering

--- Content.Shared/_Starlight/CCVar/StarlightCCVar.cs
+++ Content.Shared/_Starlight/CCVar/StarlightCCVar.cs
@@ -2,7 +2,7 @@ using Robust.Shared.Configuration;
 using Content.Shared.CCVar.CVarAccess;
 using Content.Shared.Administration;
 
-namespace Content.Shared._Starlight.CCVar;
+namespace Content.Shared.Starlight.CCVar;
 
 [CVarDefs]
 public sealed partial class StarlightCCVars

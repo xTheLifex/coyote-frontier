ecc02a518ebea4c1403adb5e4e85f3000ebededd
TheLife <nicholastoya@yahoo.com.br>
2026-04-05 15:21:24 -0300

    New Lobby Screens and Fading Between them

--- Content.Server/GameTicking/GameTicker.LobbyBackground.cs
+++ Content.Server/GameTicking/GameTicker.LobbyBackground.cs
@@ -1,6 +1,7 @@
 using Content.Server.GameTicking.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
+using Robust.Shared.Timing;
 using System.Linq;
 
 namespace Content.Server.GameTicking;
@@ -13,7 +14,7 @@ public sealed partial class GameTicker
     [ViewVariables]
     private List<ResPath>? _lobbyBackgrounds;
 
-    private static readonly string[] WhitelistedBackgroundExtensions = new string[] {"png", "jpg", "jpeg", "webp"};
+    private static readonly string[] WhitelistedBackgroundExtensions = ["png", "jpg", "jpeg", "webp"];
 
     private void InitializeLobbyBackground()
     {
@@ -21,11 +22,21 @@ public sealed partial class GameTicker
             .Select(x => x.Background)
             .Where(x => WhitelistedBackgroundExtensions.Contains(x.Extension))
             .ToList();
-
+        Timer.SpawnRepeating(30000, CycleLobbyBackground, System.Threading.CancellationToken.None);
         RandomizeLobbyBackground();
     }
 
-    private void RandomizeLobbyBackground() {
-        LobbyBackground = _lobbyBackgrounds!.Any() ? _robustRandom.Pick(_lobbyBackgrounds!).ToString() : null;
+    private void CycleLobbyBackground()
+    {
+        if (_lobbyBackgrounds == null || _lobbyBackgrounds.Count == 0)
+            return;
+
+        RandomizeLobbyBackground();
+        SendStatusToAll();
+    }
+
+    private void RandomizeLobbyBackground()
+    {
+        LobbyBackground = _lobbyBackgrounds!.Count != 0 ? _robustRandom.Pick(_lobbyBackgrounds!).ToString() : null;
     }
 }

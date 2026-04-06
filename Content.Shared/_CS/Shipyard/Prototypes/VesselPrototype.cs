dd3fdd228e96e68d874ba69a4b0866ee209f5782
DevArchwave <168038123+DevArchwave@users.noreply.github.com>
2026-04-06 17:51:02 -0300

    Bit more cleanup

--- Content.Shared/_CS/Shipyard/Prototypes/VesselPrototype.cs
+++ Content.Shared/_CS/Shipyard/Prototypes/VesselPrototype.cs
@@ -3,25 +3,26 @@ namespace Content.Shared._NF.Shipyard.Prototypes;
 public sealed partial class VesselPrototype
 {
     /// <summary>
-    ///     Extra price added per tile of the vessel when appraising/selling. Default is 1 spesos per tile. This should be changed to the LL0 price once all ships are within standards.
+    /// Extra price added per tile of the vessel when appraising/selling. Default is 1 spesos per tile. This should be changed to the LL0 price once all ships are within standards.
+    /// </summary>
     [DataField("pricePerTile")]
     public int PricePerTile = 1;
 
     /// <summary>
-    ///     Increase applied to the total appraisal/sale value (before price per tile).
-    ///     For example, a markup of 1.25 means +25% on the appraisal's value.
+    /// Increase applied to the total appraisal/sale value (before price per tile).
+    /// For example, a markup of 1.25 means +25% on the appraisal's value.
     /// </summary>
     [DataField("markup")]
     public float Markup = 1f;
 
     /// <summary>
-    ///     Is the vessel exped capable?
+    /// Is the vessel exped capable?
     /// </summary>
     [DataField("expedCapable")]
     public bool ExpedCapable = false;
 
     /// <summary>
-    ///     Is the vessel donk capable?
+    /// Is the vessel donk capable?
     /// </summary>
     [DataField("donkCapable")]
     public bool DonkCapable = false;
